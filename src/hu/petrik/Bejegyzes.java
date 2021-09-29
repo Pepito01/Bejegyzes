7fa05dea7a8aaa42cec9b3b970e9b92645e989f1
Diak <wolf.peter@petrik.hu>
2021-09-29 14:58:28 +0200

    Bejegyzes keszites

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,5 +1,12 @@
 package hu.petrik;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
     private String szerzo;
+    private String tartalom;
+    private int likeok = 0;
+    private LocalDateTime letrejott = LocalDateTime.now();
+    private LocalDateTime szerkesztve = LocalDateTime.now();
 }
